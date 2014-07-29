017fa4e854ceed9dfa5d5c9d802c498c974733ec
Asheesh Laroia <asheesh@asheesh.org>
2014-07-28 23:39:54 -0700

    Download missing modules, aieee

--- do.sh
+++ do.sh
@@ -11,6 +11,10 @@ tar zxvf debian-hurd.img.tar.gz
 # Make sure the sha1sum matches
 sha1sum debian*img | grep f40a83f105f4fffd4074867c196d798feea43468
 
+# get modules that we're missing, lulz
+wget http://download.openvz.org/kernel/branches/rhel6-2.6.32/042stab090.5/linux-image-2.6.32-openvz-042stab090.5-amd64_1_amd64.deb
+sudo dpkg -i --force-overwrite linux-image-2.6.32-openvz-042stab090.5-amd64_1_amd64.deb
+
 # FIXME: Patch out the /etc/shadow
 sudo apt-get install kpartx
 sudo depmod -a
@@ -24,9 +28,16 @@ sudo chown --reference-file=/mnt/etc/shadow /tmp/shadow
 sudo mv /tmp/shadow /mnt/etc/shadow
 sudo umount /mnt
 
+exit 0
+
 # install qemu
+sudo apt-get install qemu
 
 # run it, in the background
+qemu -redir tcp:5556::22 debian-hurd*img -curses
+
+# Wait for SSH to come online
+ssh -p 5556 root@localhost echo hello from hurd
 
 # SSH in
 
