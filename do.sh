df1f8e098b9572d375469a3eacbc936c300ce155
Asheesh Laroia <asheesh@asheesh.org>
2014-07-29 19:36:11 -0700

    twenty first century

--- do.sh
+++ do.sh
@@ -15,7 +15,7 @@ sha1sum debian*img | grep f40a83f105f4fffd4074867c196d798feea43468
 sudo apt-get install qemu
 
 # run it, in the background
-/usr/sbin/qemu -redir tcp:5556::22 debian-hurd*img -curses
+qemu-system-x86_64 -redir tcp:5556::22 debian-hurd*img -curses
 
 # FIXME: Patch out the /etc/shadow
 sudo apt-get install kpartx
